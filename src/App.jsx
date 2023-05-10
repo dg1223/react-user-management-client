6ca8c2a40a0511e15109d41522838b243a6c1a5c
dg1223 <alavi1223@hotmail.com>
2023-05-10 00:05:46 -0400

    post request - show as HTML

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     const email = form.email.value;
     const user = { name, email };
     console.log(user);
+
     fetch("http://localhost:5000/users", {
       method: "POST",
       headers: {
@@ -26,8 +27,23 @@ function App() {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("inside post response", data);
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
       });
+
+    /* fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("inside post response", data);
+      }) */
   };
 
   return (
